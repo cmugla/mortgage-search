99496f3d1d31be04ff8493edcca87a0450d2be6b
Celeste Glavin <cmugla@gmail.com>
2017-05-15 09:57:41 -0400

    show results based on an input

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -7,15 +7,56 @@ import AjaxAdapter from '../helpers/ajaxAdapter.js'
 const ajax = new AjaxAdapter(fetch)
 
 export default class App extends Component {
-  state={}
+  state={
+    loanAmount: '',
+    results: null,
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    ajax.getMortgageData(this.state.loanAmount).then(data => {
+      this.setState({
+        results: data
+      })
+    })
+  }
+
+  updateLoanAmt = e => {
+    this.setState({
+      loanAmount: e.target.value
+    })
+  }
 
   render () {
     console.log("you're excellent");
     console.log("\n.-        -.\n| ,-. ,-.  |\n| |   | |  |\n| `-' `-|  |\n`-     ,| -'\n       `'    ")
 
+    const { loanAmount, results } = this.state
+
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Mortgage Search and Sort</h1>
+        <form onSubmit={this.handleSubmit} >
+          <input placeholder="Enter loan amount" value={loanAmount} onChange={this.updateLoanAmt} />
+          <button type="submit">SEARCH</button>
+        </form>
+        <ul>
+          {
+            results
+            &&
+            results.length > 0
+            &&
+            results.map((each, i) => (
+              <li key={`results-${i}`}>{each.lender.name}</li>
+            ))
+          }
+          {
+            (!results || !results.length)
+            &&
+            <li>No results.</li>
+          }
+        </ul>
       </div>
     )
   }
